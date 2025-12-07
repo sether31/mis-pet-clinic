ed66d710de098f626499305800aa0bb9db45eb80
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-12-07 13:38:09 +0800

    feat: Finish login with jwt and email based otp
    - Create jwt for token and security.
    - Create otp service.

--- backend-api/service/MailService.php
+++ backend-api/service/MailService.php
@@ -7,42 +7,42 @@ use PHPMailer\PHPMailer\Exception;
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-function sendOTP($to_email, $otp_code, $name, $purpose, $expires_at) {
-$email = new PHPMailer(true);
+function sendMailOtp($to_email, $otp_code, $name, $purpose, $expires_at) {
+  $email = new PHPMailer(true);
 
-try {
-  $email->isSMTP();
-  $email->Host = $_ENV['SMTP_HOST'];
-  $email->Username = $_ENV['SMTP_USER'];
-  $email->Password = $_ENV['SMTP_PASS'];
-  $email->Port = $_ENV['SMTP_PORT'];
-  $email->SMTPAuth = true;
-  $email->SMTPSecure = "tls";
+  try {
+    $email->isSMTP();
+    $email->Host = $_ENV['SMTP_HOST'];
+    $email->Username = $_ENV['SMTP_USER'];
+    $email->Password = $_ENV['SMTP_PASS'];
+    $email->Port = $_ENV['SMTP_PORT'];
+    $email->SMTPAuth = true;
+    $email->SMTPSecure = "tls";
 
-  // recipient
-  $email->setFrom($_ENV['SMTP_FROM_EMAIL'], $_ENV['SMTP_FROM_NAME']);
-  $email->addAddress($to_email, $name);
+    // recipient
+    $email->setFrom($_ENV['SMTP_FROM_EMAIL'], $_ENV['SMTP_FROM_NAME']);
+    $email->addAddress($to_email, $name);
 
-  // content
-  $email->isHTML(true);
-  $email->Subject = $_ENV['SMTP_FROM_NAME']. strtoupper($purpose) . " OTP Code";
-  $email->Body = "
-    <p>Hello <strong>$name</strong>,</p>
-    <p>
-      Your OTP code for $purpose is: 
-      <strong>$otp_code</strong>
-    </p>
-    <p>This code will be expired at $expires_at</p>
-    <br>
-    <p>
-      Best regards,
+    // content
+    $email->isHTML(true);
+    $email->Subject = $_ENV['SMTP_FROM_NAME'] . ' ' . strtoupper($purpose) . " OTP Code";
+    $email->Body = "
+      <p>Hello <strong>$name</strong>,</p>
+      <p>
+        Your OTP code for $purpose is: 
+        <strong>$otp_code</strong>
+      </p>
+      <p>This code will be expired at $expires_at</p>
       <br>
-      {$_ENV['SMTP_FROM_NAME']}
-    </p>
-    ";
-  $email->send();
-} catch(Exception $e) {
-  echo "error: {$email->ErrorInfo}";
-}
+      <p>
+        Best regards,
+        <br>
+        {$_ENV['SMTP_FROM_NAME']}
+      </p>
+      ";
+    $email->send();
+  } catch(Exception $e) {
+    echo "error: {$email->ErrorInfo}";
+  }
 }
 ?>
