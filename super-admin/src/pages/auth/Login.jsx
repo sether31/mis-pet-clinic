4b94bcef8636095efa81ffd6ecd336c4b6ae6bbc
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-12-16 08:17:26 +0800

    setup: Setup super admin environment
    - Install and setup tailwind css and create components.
    - Install and setup react router dom.
    - Install framer motion, jwt decode and react toastify.

---
+++ super-admin/src/pages/auth/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>
+      <h1 className='text-5xl text-center font-bold mt-5 text-[var(--clr-dark-green)]'>TEST</h1>
+    </div>
+  )
+}
