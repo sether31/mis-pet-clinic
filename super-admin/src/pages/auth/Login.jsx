a0371b0d97fee657307da4217d3c080617be3308
Seth Hernandez <sethmichaelhernandez@gmail.com>
2026-03-26 22:09:50 +0800

    fix(general): Implement dynamic login photo in the UI

--- super-admin/src/pages/auth/Login.jsx
+++ super-admin/src/pages/auth/Login.jsx
@@ -189,7 +189,6 @@ export default function Login() {
     hidden: { opacity: 0, y: 20, scale: 0.95 },
     visible: { opacity: 1, y: 0, scale: 1, transition: { type: "spring", stiffness: 100 } },
   };
-  
 
   return (
     <>
@@ -204,7 +203,7 @@ export default function Login() {
             transition={{ duration: 1.2, ease: "easeOut" }}
           >
             <motion.img 
-              src={loginPic} 
+              src={platformData?.login_photo ? `${API_URL}/${platformData.login_photo}` : loginPic}  
               alt="login picture 2 dogs" 
               className='absolute h-[95%] w-full rounded-xl bottom-0 right-0 object-cover'
               initial={{ y: 50, opacity: 0 }}
